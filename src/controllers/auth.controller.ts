91dd04fb6cbc2954f7deb50a25367ac0d78c97ae
rahulch49 <chrahul2000@gmail.com>
2025-08-27 20:45:43 +0530

    changes test

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -17,7 +17,7 @@ export const signup = async (req: FastifyRequest<{ Body: SignupBody }>, reply: F
                 password
             }
         });
-        console.log('user--', newUser);
+        console.log('user-->>', newUser);
         return {
         message: "Signup route hit!"
     }
