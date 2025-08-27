38f731a429e7f9950ce66666cd5d7bac51e7c5ab
rahulch49 <chrahul2000@gmail.com>
2025-08-27 19:05:16 +0530

    Update auth.controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,30 @@
 import { FastifyRequest, FastifyReply } from "fastify";
+import * as bcrypt from "bcrypt";
+import { ERROR500 } from "../utils/constants.js";
+import { SignupBody } from "../utils/interfaces.js";
+import { prisma } from "../utils/prisma.js";
 
-export const signup = async (req: FastifyRequest, reply: FastifyReply) => {
-    console.log("First console!");
-    return {
+export const signup = async (req: FastifyRequest<{ Body: SignupBody }>, reply: FastifyReply) => {
+    try{
+        const { body } = req;
+        const { name, email, phone, password } = body;
+        console.log(name, email, phone, password);
+        const newUser = await prisma.user.create({
+            data: {
+                name,
+                email,
+                phone,
+                password
+            }
+        });
+        console.log('user--', newUser);
+        return {
         message: "Signup route hit!"
     }
+    }
+    catch (err: any){
+        console.log('err', err);
+        return reply.code(ERROR500.statusCode).send({...ERROR500, ...{message: err.message}});
+    }
+    
 }
